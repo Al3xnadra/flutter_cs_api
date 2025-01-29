4c1be75960d2b007f602bf4be342a9f43576d0da
Aleksandra Balon <a.f.w.783@gmail.com>
2025-01-29 20:23:02 +0100

    Add segment for skin float row

--- lib/features/skins/skin_details/widgets/skin_float.dart
+++ lib/features/skins/skin_details/widgets/skin_float.dart
@@ -11,12 +11,13 @@ class SkinFloat extends StatelessWidget {
     return Flexible(
       child: Container(
         width: double.infinity,
-        padding: EdgeInsets.all(10),
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
         decoration: BoxDecoration(
           color: Color(0xFF2D2D2D),
           borderRadius: BorderRadius.circular(15),
         ),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -34,6 +35,7 @@ class SkinFloat extends StatelessWidget {
             Stack(
               children: [
                 Container(
+                  width: 265,
                   height: 3,
                   decoration: BoxDecoration(color: Colors.red),
                 ),
@@ -44,7 +46,66 @@ class SkinFloat extends StatelessWidget {
                 ),
               ],
             ),
+            RowSegmentFloat(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class RowSegmentFloat extends StatelessWidget {
+  RowSegmentFloat({
+    super.key,
+  });
+
+  final List<Map<String, dynamic>> segments = [
+    {
+      'name': 'FN',
+      'message': 'Factory New: 0.00 - 0.07',
+    },
+    {
+      'name': 'MW',
+      'message': 'Miminal Wear: 0.07 - 0.15',
+    },
+    {
+      'name': 'FT',
+      'message': 'Field-Tested: 0.15 - 0.38',
+    },
+    {
+      'name': 'WW',
+      'message': 'Well-Worn: 0.38 - 0.45',
+    },
+    {
+      'name': 'BS',
+      'message': 'Battle-Scarred: 0.45 - 1.00',
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(
+        segments.length,
+        (index) => Container(
+          width: 53,
+          height: 20,
+          decoration: BoxDecoration(
+            color: Colors.grey[700],
+            border: index != segments.length - 1
+                ? Border(
+                    right: BorderSide(
+                      color: Color(0xFF2D2D2D),
+                    ),
+                  )
+                : null,
+          ),
+          child: Center(
+            child: Tooltip(
+                message: segments[index]['message'],
+                child: Text(segments[index]['name'])),
+          ),
         ),
       ),
     );
